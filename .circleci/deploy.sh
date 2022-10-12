e12e04311bf5a088e801f07ca9ef2cc5a5ed007d
Senthil <Thyagarajan.Senthil@bcg.com>
2022-10-12 15:49:47 +0530

    running a bash script 33

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "Pulling a image"
 docker pull $AWS_ECR_ACCOUNT_URL/temp
+echo "Pull complete"
 # docker stop $AWS_ECR_ACCOUNT_URL/temp
 # docker rm $AWS_ECR_ACCOUNT_URL/temp
 docker run -p 80:3000 -d $AWS_ECR_ACCOUNT_URL/temp
