25e0eb42294cec80600eb0ca5592a1c9a49568b3
Senthil <Thyagarajan.Senthil@bcg.com>
2022-10-12 17:00:41 +0530

    deploy sh file changed

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,6 +5,16 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 echo "Pulling a image"
 docker pull 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
 echo "Pull complete"
+
+if [ "$( docker container inspect -f '{{.State.Status}}' 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest )" == "running" ];
+
+then
+docker stop 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
+docker rm 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
+
+fi
+
+
 # docker stop $AWS_ECR_ACCOUNT_URL/temp
 # docker rm $AWS_ECR_ACCOUNT_URL/temp
 docker run -p 80:3000 -d 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
