ac63d7a125aa280c17579b17dead1d4bb92cad69
Senthil <Thyagarajan.Senthil@bcg.com>
2022-10-12 16:00:47 +0530

    sh command

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+echo "logging in"
+docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) ec2-3-108-66-178.ap-south-1.compute.amazonaws.com
 echo "Pulling a image"
-docker pull $AWS_ECR_ACCOUNT_URL/temp
+docker pull ec2-3-108-66-178.ap-south-1.compute.amazonaws.com/temp
 echo "Pull complete"
 # docker stop $AWS_ECR_ACCOUNT_URL/temp
 # docker rm $AWS_ECR_ACCOUNT_URL/temp
-docker run -p 80:3000 -d $AWS_ECR_ACCOUNT_URL/temp
+docker run -p 80:3000 -d ec2-3-108-66-178.ap-south-1.compute.amazonaws.com/temp
