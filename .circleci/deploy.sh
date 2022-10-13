fa4900a2e6e8eecaeb09f24721a030d2ae402784
senthil <Thyagarajan.Senthil@bcg.com>
2022-10-13 16:45:14 +0530

    shell script changes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,12 +8,18 @@ echo "Pull complete"
 
 CONTAINER_NAME='sales-frontend'
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+EXIT=$(docker ps -q -f status=exited -f name=^/${CONTAINER_NAME}$)
 if [ "${CID}" ]; then
   echo "Container exists"
   docker stop sales-frontend
   docker rm sales-frontend
 fi
 
+if [ "${EXIT}" ]; then
+  echo "Container exists"
+  docker rm sales-frontend
+fi
+
 # docker stop $AWS_ECR_ACCOUNT_URL/temp
 # docker rm $AWS_ECR_ACCOUNT_URL/temp
-docker run --name sales-frontend -p 80:3000 -d 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
+docker run --name sales-frontend -p 3000:3000 -d 764941139426.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
